e8a75bacbb2d2da62762c8cab1d3024cab382a07
praveen-kumar0101 <126513506+praveen-kumar0101@users.noreply.github.com>
2023-10-02 16:35:34 +0530

    second commit

---
+++ add.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Input numbers
+num1=5
+num2=7
+
+# Perform addition using `expr`
+result=$(expr $num1 + $num2)
+
+# Display the result
+echo "The sum of $num1 and $num2 is: $result"
